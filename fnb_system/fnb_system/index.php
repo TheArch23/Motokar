a118ab55d32ea687856569f48c299d5c862e1de2
imrantafa <135511588+imrantafa@users.noreply.github.com>
2023-08-20 17:36:19 +0800

    Delete fnb_system/fnb_system directory

--- fnb_system/fnb_system/index.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="save_location.php">
-  Name: <input type="text" name="name"><br>
-  Address: <input type="text" name="address"><br>
-  Latitude: <input type="text" name="latitude"><br>
-  Longitude: <input type="text" name="longitude"><br>
-  <input type="submit" value="Save">
-</form>
